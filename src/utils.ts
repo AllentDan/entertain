dde43942a1277d9c236890e993f6dff7a522054e
AllentDan <AllentDan@yeah.net>
2022-02-14 17:05:48 +0800

    add save and load functions

--- src/utils.ts
+++ src/utils.ts
@@ -7,3 +7,32 @@ export class Util {
     }
 
 }
+
+export function _saveTodo(filePath: string, todos: []) {
+    const fs = require('fs');
+
+    // convert JSON object to string
+    const data = JSON.stringify(todos);
+
+    // write JSON string to a file
+    fs.writeFile(filePath, data, (err: string) => {
+        if (err) {
+            throw err;
+        }
+    });
+}
+
+export function _loadTodo(filePath: string) {
+    const fs = require('fs');
+
+    // read JSON object from file
+    fs.readFile(filePath, 'utf-8', (err: string, data: string) => {
+        if (err) {
+            throw err;
+        }
+
+        // parse JSON object
+        const todos = JSON.parse(data.toString());
+        return todos;
+    });
+}
